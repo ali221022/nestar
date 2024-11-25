da22cdfdb6469c86fc0ec11ef57f01dcb6c86862
ali221022 <shohjahon.juraev.4496@mail.ru>
2024-11-25 11:39:51 +0900

    BRR: feat: create member schema model

--- apps/nestar-api/src/app.resolver.ts
+++ apps/nestar-api/src/app.resolver.ts
@@ -1,10 +1,12 @@
 import { Query, Resolver } from "@nestjs/graphql";
+import { AppService } from "./app.service";
 
 
 @Resolver()
 export class AppResolver {
+    constructor(private readonly appService: AppService) {}
     @Query(() => String)
     public sayHello(): string {
-        return 'GraphQL API Server';
+        return this.appService.getHello();
     }
 }
--- apps/nestar-batch/src/nestar-batch.module.ts
+++ apps/nestar-batch/src/nestar-batch.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { NestarBatchController } from './nestar-batch.controller';
 import { NestarBatchService } from './nestar-batch.service';
 
+
 @Module({
   imports: [],
   controllers: [NestarBatchController],
-  providers: [NestarBatchService],
+  providers: [NestarBatchService, ],
 })
 export class NestarBatchModule {}
