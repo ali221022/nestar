d30f0aa034e54b8ab9cc1272c6cd903e95c7dd1e
ali221022 <shohjahon.juraev.4496@mail.ru>
2024-11-28 17:44:32 +0900

    BRR: feat: develop logging via global interceptors

--- apps/nestar-api/src/app.resolver.ts
+++ apps/nestar-api/src/app.resolver.ts
@@ -4,9 +4,8 @@ import { AppService } from "./app.service";
 
 @Resolver()
 export class AppResolver {
-    constructor(private readonly appService: AppService) {}
     @Query(() => String)
     public sayHello(): string {
-        return this.appService.getHello();
+        return "GraphQL API Server!";
     }
 }
