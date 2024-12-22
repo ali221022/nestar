4d438c8afd5789864fdaa6da26795d9a53622337
ali221022 <shohjahon.juraev.4496@mail.ru>
2024-12-23 00:35:04 +0900

    BRR: feat: develop getFavorites graphQL api

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -1,7 +1,7 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { PropertyService } from './property.service';
 import { Properties, Property } from '../../libs/dto/property/property';
-import { AgentPropertiesInquiry, AllPropertiesInquiry, PropertiesInquiry, PropertyInput } from '../../libs/dto/property/property.input';
+import { AgentPropertiesInquiry, AllPropertiesInquiry, OrdinaryInquiry, PropertiesInquiry, PropertyInput } from '../../libs/dto/property/property.input';
 import { MemberType } from '../../libs/enums/member.enum';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { UseGuards } from '@nestjs/common';
@@ -56,6 +56,16 @@ export class PropertyResolver {
   @UseGuards(WithoutGuard)
    @Query((returns) => Properties)
    public async getProperties(
+    @Args("input") input: OrdinaryInquiry,
+    @AuthMember("_id") memberId: ObjectId,
+  ): Promise<Properties> {
+    console.log("Query: getFavorites");
+    return await this.propertyService.getFavorites(memberId, input);
+  }
+
+  @UseGuards(AuthGuard)
+   @Query((returns) => Properties)
+   public async getFavorites(
     @Args("input") input: PropertiesInquiry,
     @AuthMember("_id") memberId: ObjectId,
   ): Promise<Properties> {
--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -5,7 +5,7 @@ import { AuthService } from '../auth/auth.service';
 import { Model, ObjectId } from 'mongoose';
 import { Properties, Property } from '../../libs/dto/property/property';
 import { Direction, Message } from '../../libs/enums/common.enum';
-import { AgentPropertiesInquiry, AllPropertiesInquiry, PropertiesInquiry, PropertyInput } from '../../libs/dto/property/property.input';
+import { AgentPropertiesInquiry, AllPropertiesInquiry, OrdinaryInquiry, PropertiesInquiry, PropertyInput } from '../../libs/dto/property/property.input';
 import { MemberService } from '../member/member.service';
 import { PropertyStatus } from '../../libs/enums/property.enum';
 import { StatisticModifier, T } from '../../libs/types/common';
@@ -167,6 +167,10 @@ private shapeMatchQuery(match: T, input: PropertiesInquiry): void {
         });
     }
 }
+
+public async getFavorites(memberId: ObjectId, input: OrdinaryInquiry): Promise<Properties>  {
+     return await this.likeService.getFavoriteProperties(memberId, input);
+};
 
 public async getAgentProperties(memberId: ObjectId, input: AgentPropertiesInquiry): Promise<Properties> {
     const { propertyStatus } = input.search;
