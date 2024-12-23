cf95cf9cd8841ae50722c58d36304bb60a886911
ali221022 <shohjahon.juraev.4496@mail.ru>
2024-12-23 23:19:34 +0900

    BRR: feat: create WebSocket Connection process

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -1,9 +1,11 @@
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { LoggingInterceptor } from './libs/interceptor/Logging.interceptor';
 import { graphqlUploadExpress } from "graphql-upload";
 import * as express from "express";
+import { WsAdapter } from '@nestjs/platform-ws';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +14,7 @@ async function bootstrap() {
   app.enableCors({ origin: true, credentials: true });
   app.use(graphqlUploadExpress({ maxFileSize: 15000000, maxFile: 10 }));
   app.use("/uploads", express.static("./uploads"));
+  app.useWebSocketAdapter(new WsAdapter(app));
   await app.listen(process.env.PORT_API ?? 3000);
 }
 bootstrap();
